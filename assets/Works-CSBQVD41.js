9df2fcb8a34cbc076ba18de78095cac6a928147e
Charllys de Lima <charllys.lima@outlook.com>
2024-07-20 04:18:25 +0000

    Updates

---
+++ assets/Works-CSBQVD41.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BJ-1m3YS.js";const t=()=>r.jsx("div",{className:"rounded-3xl p-10 dark:text-white",children:r.jsx("h1",{children:"Em construção"})});export{t as default};
--- assets/Works-Cc61Cqde.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CKGas8Ln.js";const t=()=>r.jsx("div",{className:"rounded-3xl p-10 dark:text-white",children:r.jsx("h1",{children:"Em construção"})});export{t as default};
